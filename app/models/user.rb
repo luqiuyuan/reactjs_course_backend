31d71dffc22d5590c3d2a475da589e295300ac92
Lu Sun <lsun100@uottawa.ca>
2018-11-29 17:33:58 -0500

    Accomplish a basic user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,62 @@
 class User < ApplicationRecord
+
+  # The model user has secure password
+  has_secure_password
+
+  # ==========================
+  # Constants
+  # ==========================
+
+  EMAIL_LENGTH_MAX = 255
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  PASSWORD_LENGTH_MIN = 6
+  PASSWORD_LENGTH_MAX = 50
+  NAME_LENGTH_MAX = 50
+
+  # ==========================
+  # Validations
+  # ==========================
+
+  validates :email, presence: true, length: {maximum: EMAIL_LENGTH_MAX}, uniqueness: true,
+                    format: {with: VALID_EMAIL_REGEX}
+  validates :password, presence: true, length: {minimum: PASSWORD_LENGTH_MIN, maximum: PASSWORD_LENGTH_MAX}, on: :create
+  validates :password, allow_nil: true, length: {minimum: PASSWORD_LENGTH_MIN, maximum: PASSWORD_LENGTH_MAX}, on: :update
+  validate :password_validation
+  validates :name, length: {maximum: NAME_LENGTH_MAX}
+
+  # ==========================
+  # Custom Validations
+  # ==========================
+  
+  def password_validation
+    # If password is pure upcase
+    if !self.password.blank? && self.password.upcase == self.password
+      self.errors.add(:password, :should_contain_downcase)
+    end
+
+    # If password is pure downcase
+    if !self.password.blank? && self.password.downcase == self.password
+      self.errors.add(:password, :should_contain_upcase)
+    end
+  end
+  
+  # ==========================
+  # Callbacks
+  # ==========================
+
+  before_save {
+    self.email = email.downcase
+  }
+
+  # ==========================
+  # Custom Methods
+  # ==========================
+
+  # return the has digest of the given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,80 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(email: "test@bigfish.com", password: "123456aA", name: "Mr. Somebody")
+  end
+
+  test "valid model should ba valid" do
+    assert @user.valid?
+  end
+
+  test "model without email should not be valid" do
+    @user.email = nil
+    assert_not @user.valid?
+  end
+
+  test "model with email that is too long should not be valid" do
+    @user.email = "a" * (User::EMAIL_LENGTH_MAX - 3) + "@a.a"
+    assert_not @user.valid?
+  end
+
+  test "model with illegal email format should not be valid" do
+    @user.email = "a@a@a.a"
+    assert_not @user.valid?
+  end
+
+  test "model without password should not be valid" do
+    @user.password = nil
+    assert_not @user.valid?
+  end
+
+  test "model with password that is too short should not be valid" do
+    @user.password = "a" * (User::PASSWORD_LENGTH_MIN - 1)
+    assert_not @user.valid?
+  end
+
+  test "model with password that is too long should not be valid" do
+    @user.password = "a" * (User::PASSWORD_LENGTH_MAX + 1)
+    assert_not @user.valid?
+  end
+
+  test "model with password that does not contain downcase letters should not be valid" do
+    @user.password = "ABCDEF"
+    assert_not @user.valid?
+  end
+
+  test "model with password that does not contain upcase letters should not be valid" do
+    @user.password = "abcdef"
+    assert_not @user.valid?
+  end
+
+  test "model without name should be valid" do
+    @user.name = nil
+    assert @user.valid?
+  end
+
+  test "model with name that is too long should not be valid" do
+    @user.name = "a" * (User::NAME_LENGTH_MAX + 1)
+    assert_not @user.valid?
+  end
+
+  test "model without password should be valid on update" do
+    user = users(:guojing)
+    assert user.valid?
+  end
+
+  test "valid model should be valid on update" do
+    user = users(:guojing)
+    user.password = "123456aA"
+    assert user.valid?
+  end
+
+  test "model with password that is too short should not be valid on update" do
+    user = users(:guojing)
+    user.password = "a" * (User::PASSWORD_LENGTH_MIN - 1)
+    assert_not user.valid?
+  end
+
 end
