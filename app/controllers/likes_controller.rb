5fc737b5109ef4004e42ee04a65341a53ff1209d
Lu Sun <lsun100@uottawa.ca>
2018-12-25 13:52:36 +0800

    Finish API POST /questions/:question_id/like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,22 +1,33 @@
 class LikesController < ApplicationController
 
+  before_action :set_user_logged_in, only: [:create, :destroy]
   before_action :set_question, only: [:create, :destroy]
-  before_action :set_like, only: [:create, :destroy]
 
   # POST /questions/:question_id/like
   def create
-    @like = Like.new()
-
-    # if @like.save
-    #   render json: @like, status: :created, location: @like
-    # else
-    #   render json: @like.errors, status: :unprocessable_entity
-    # end
+    @like = Like.new(user: @user_logged_in, likable: @question)
+    if @like.save
+      render json: { like: @like }, status: :created
+    else
+      @errors = translateModelErrors @like
+      add_prefix_to_field @errors, "like:"
+      render json: { errors: @errors }, status: :bad_request
+    end
   end
 
   # DELETE /questions/:question_id/like
   def destroy
-    # @like.destroy
+    @like = Like.find_by(user: @user_logged_in, likable: @question)
+  rescue ActiveRecord::RecordNotFound
+    render status: :not_found
   end
+
+  private
+
+    def set_question
+      @question = Question.find(params[:question_id])
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found
+    end
 
 end
--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -2,6 +2,30 @@ require 'test_helper'
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
   
-  
+  test "should create like if not exist" do
+    post question_like_url(questions(:two)), headers: login_as(:guojing)
+
+    assert_response :created
+  end
+
+  test "should not create like if exist" do
+    post question_like_url(questions(:one)), headers: login_as(:guojing)
+
+    assert_response :bad_request
+    assert hash_included_unordered({
+      errors: [
+        {
+          code: "duplicated_field",
+          field: "like:user"
+        }
+      ]
+    }, json_response)
+  end
+
+  test "should not like a non-existent question" do
+    post question_like_url(1234567), headers: login_as(:guojing)
+
+    assert_response :not_found
+  end
 
 end
