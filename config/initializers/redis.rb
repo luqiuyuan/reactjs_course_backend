b2da2c980cf255b07100ace469347a5367a89ff1
luqiuyuan <lsun100@uottawa.ca>
2019-03-01 14:26:21 -0500

    Setup redis configuration

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+$redis = Redis.new(:host => 'redis-10383.c13.us-east-1-3.ec2.cloud.redislabs.com', :port => 10383, :password => 'EeygHB6hZQzciyulpm3Ztv1o0aPz45Tz')
