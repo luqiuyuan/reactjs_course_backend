4e1087c43869d4c0b286f79aa72703182e7303b6
luqiuyuan <lsun100@uottawa.ca>
2019-03-01 14:28:51 -0500

    Update redis configuration in order to user env variables

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,4 @@
-$redis = Redis.new(:host => 'redis-10383.c13.us-east-1-3.ec2.cloud.redislabs.com', :port => 10383, :password => 'EeygHB6hZQzciyulpm3Ztv1o0aPz45Tz')
+if ENV["REDISCLOUD_URL"]
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
+    $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
